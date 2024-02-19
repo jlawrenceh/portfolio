8e42a97988a6c32be41d44f9c162569227493431
jlawrenceh <jlawrencehernandez@gmail.com>
2024-02-20 05:44:59 +0800

    Added About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
-
+import About from "./components/About";
 function App() {
  
 
@@ -12,6 +12,7 @@ function App() {
       
       <main className="main">
         <Home/>
+        <About/>
       </main>
       
     </>
