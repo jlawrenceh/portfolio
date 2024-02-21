c89fffbb9d84c06e5952c20437bc8bdf9a398401
jlawrenceh <jlawrencehernandez@gmail.com>
2024-02-22 06:58:25 +0800

    Added header - not yet responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
-
 import Home from "./components/Home/Home";
-
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import Services from "./components/Services/Services";
-import Qualifications from "./components/Qualifications/Qualifications";
-import Testimonials from "./components/Testimonials/Testimonials";
-import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import Scrollup from "./components/Scrollup/Scrollup";
 
 
 function App() {
@@ -22,17 +13,9 @@ function App() {
       <Header/>
       
       <main className="main">
-        <Home/>
-        <About/>
-        <Skills/>
-        <Services/>
-        <Qualifications/>
-        <Testimonials />
-        <Contact/>
+       
       </main>
-
-      <Footer/>
-      <Scrollup/>
+  
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,109 +4,22 @@ import "./Header.css";
 
 function Header() {
 
-    window.addEventListener("scroll",function () {
-        const header = document.querySelector(".header");
-        // when the scroll is higher than 200 viewport height,
-        // add the scroll-header class to a tag with header tag.
-        if (this.scrollY >= 80)
-        { header.classList.add("scroll-header");}
-        else {
-            header.classList.remove("scroll-header");
-        }
-    });
-    
-
-
-    /*====== Toggle Menu =======*/
-    const[Toggle, showMenu] = useState(false);
-    const [activeNav, setActiveNav] = useState("#home");
-
   return (
     <header className="header">
         <nav className="nav container">
-            <a href="#" className="nav__logo">
-                LOGO
-            </a>
-
-            <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-                <ul className="nav__list grid">
-                    <li className="nav__item">
-                        <a href="#home" 
-                        onClick={() => setActiveNav("#home")}
-                        className={activeNav === "#home" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-
-                           <i className="uil uil-estate nav__icon"></i>
-                           Home
-                        </a>
-                    </li>
-
-                    <li className="nav__item">
-                        <a href="#about"
-                        onClick={() => setActiveNav("#about")}
-                        className={activeNav === "#about" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-                           <i className="uil uil-user nav__icon"></i>
-                           About
-                        </a>
-                    </li>
-
-                    <li className="nav__item">
-                        <a href="#skills"
-                        onClick={() => setActiveNav("#skills")}
-                        className={activeNav === "#skills" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-                           <i className="uil uil-file-alt nav__icon"></i>
-                           Skills
-                        </a>
-                    </li>
-
-                    <li className="nav__item">
-                        <a href="#services" 
-                        onClick={() => setActiveNav("#services")}
-                        className={activeNav === "#services" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-                           <i className="uil uil-briefcase nav__icon"></i>
-                           Services
-                        </a>
-                    </li>
-
-                    <li className="nav__item">
-                        <a href="#portfolio" 
-                        onClick={() => setActiveNav("#portfolio")}
-                        className={activeNav === "#portfolio" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-                           <i className="uil uil-scenery nav__icon"></i>
-                           Portfolio
-                        </a>
-                    </li>
-
-                    <li className="nav__item">
-                        <a href="#contact" 
-                        onClick={() => setActiveNav("#contact")}
-                        className={activeNav === "#contact" 
-                            ? "nav__link active-link"
-                            :  "nav__link" }>
-                           <i className="uil uil-message nav__icon"></i>
-                           Contact
-                        </a>
-                    </li>
-                </ul>
-        
-                <i className="uil uil-times nav__close" onClick={() =>
-                     showMenu(!Toggle)}>    
-                </i>
-            </div>
-
-            <div className="nav__toggle" onClick={ () => showMenu(!Toggle)}>
-                <i className="uil uil-apps"></i>
-            </div>
-
+            
+                <a href="#" className="nav_logo"> 
+                    LOGO 
+                </a>
+                <div className="nav_menu">
+                        <ul className="nav_list">
+                            <li><a href="#" className="nav_link">Home</a></li>
+                            <li><a href="#" className="nav_link">About</a></li>
+                            <li><a href="#" className="nav_link">Projects</a></li>
+                            <li><a href="#" className="nav_link">Certifications</a></li>
+                            <li><a href="#" className="nav_link">Contact</a></li>
+                        </ul>
+                </div>
         </nav>
     </header>
   )
